b7be161e5d09358b29dcd33494d61c1726fc27c1
mdc <mdc@bjpowernode.com>
2023-05-09 17:08:57 +0800

    初始化

---
+++ src/main/java/com/qiancheng/y/CommonUploadUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.qiancheng.y;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonUploadUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonUploadUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qiancheng/y/CommonUploadUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiancheng.y;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUploadUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
